c549acd8e87e67eb813b845f0576b667983874d7
lucytang4 <ltang@stuy.edu>
2016-06-03 00:58:48 +0000

    files

---
+++ President.java
@@ -0,0 +1,7 @@
+public class President{
+    //instance vars
+    boolean dead, foughtWar, scandal, prezSib, naturalDeath,;
+    String party;
+    
+    //checks instance var against string question
+}
