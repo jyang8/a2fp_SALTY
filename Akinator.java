45e8a57205ccb12e2510324d8f519c9647dceb19
jyang8 <jyang8@stuy.edu>
2016-06-06 14:50:52 -0400

    Update Akinator.java

--- Akinator.java
+++ Akinator.java
@@ -2,4 +2,8 @@ public class Akinator{
     //instance vars
     PriorityQueue<President> posAnswers, rejAnswers, idkAnswers;
     PriorityQueue<String> questions, idkQuestions;
+    
+    public Akinator() 
+    {
+    }
 }
